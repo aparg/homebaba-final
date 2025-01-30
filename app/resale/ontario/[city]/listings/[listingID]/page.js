06143c2f73386921c7064c58adc8bc05ab461525
aparg <apargtm@gmail.com>
2025-01-30 19:55:46 +0545

    style: buttons color changed

--- app/resale/ontario/[city]/listings/[listingID]/page.js
+++ app/resale/ontario/[city]/listings/[listingID]/page.js
@@ -125,12 +125,12 @@ const page = async ({ params }) => {
               <div className="hidden sm:block relative w-full">
                 <Gallery data={imageURLs} />
                 <div className="space-x-2 order-2 sm:order-1 absolute bottom-2 left-2">
-                  <button className="bg-black p-1 text-white text-xs font-bold mt-1 mb-2 sm:my-0 w-fit-content rounded-md">
+                  <button className="bg-resale-primary-color p-1 text-black text-xs font-bold mt-1 mb-2 sm:my-0 w-fit-content rounded-md">
                     <TimeAgo
                       modificationTimestamp={main_data.OriginalEntryTimestamp}
                     />
                   </button>
-                  <button className="bg-black p-1 text-white text-xs font-bold mt-1 mb-2 sm:my-0 w-fit-content rounded-md">
+                  <button className="bg-resale-primary-color p-1 text-black text-xs font-bold mt-1 mb-2 sm:my-0 w-fit-content rounded-md">
                     <span>{main_data.PropertySubType}</span>
                   </button>
                 </div>
